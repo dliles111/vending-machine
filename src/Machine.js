4f57a48a30d984cbf70af2a53aac4b35853e25dc
Dylan Liles <dylans@Dylan-Liless-MacBook-Pro.local>
2020-11-25 00:37:26 +0900

    This dsplays the item is unavailable, and the deposit is insufficient

--- src/Machine.js
+++ src/Machine.js
@@ -1,18 +1,49 @@
 class Machine {
   constructor() {
-    this.snacks = []
+    this.snacks = [];
+    this.balance = 0;
   }
 
   seeSelections() {
-    return this.snacks
+    return this.snacks;
   }
 
   stock(inventory) {
-    if(inventory == undefined) {
-      throw Error("please do not troll. you cannot stock nothing.")
+    if (inventory == undefined) {
+      throw Error("please do not troll. you cannot stock nothing.");
     }
-    this.snacks = inventory
+    this.snacks = inventory;
+  }
+
+  deposit(amount) {
+    if (
+      amount == 10 ||
+      amount == 20 ||
+      amount == 50 ||
+      amount == 100 ||
+      amount == 500
+    ) {
+      this.balance = this.balance + amount;
+    } else {
+      throw Error("Invalid bill type");
+    }
+    
+    return `You have deposited Rs ${this.balance}`;
+  }
+
+  selectItem(snackName) {
+    for (var i = 0; i < this.snacks.length; i++) {
+      if (this.snacks[i].name == snackName) {
+          if (this.snacks[i].price > this.balance) {
+            return `Your deposit is insufficient. Please add Rs ${this.snacks[i].price - this.balance} for this item`;
+          } else {
+            return "heres ur item";
+          }
+      }
+    }
+
+    return "The item you selected is unavailable";
   }
 }
 
-module.exports = Machine
+module.exports = Machine;
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -27,18 +27,6 @@ describe('The vending machine', () => {
     expect(vendingMachine.seeSelections()).toEqual([snack])
   })
 
-  // someone attempts to stock without inventory
-  // it('stocks nothing if there is no inventory passed', () => {
-  //   // setup
-  //   const vendingMachine = new Machine()
-
-  //   // exercise
-  //   vendingMachine.stock()
-
-  //   // assertion
-  //   expect(vendingMachine.seeSelections()).toEqual([])
-  // })
-
   it('displays an error if no inventory comes with stocking', () => {
     // setup
     const vendingMachine = new Machine()
@@ -47,4 +35,55 @@ describe('The vending machine', () => {
     // exercise & assert
     expect(() => vendingMachine.stock()).toThrow(displayMessage)
   })
+
+  // it('displays how much money was deposited', () => {
+  //   const vendingMachine = new Machine();
+  //   const displaysAmount = 'You have deposited Rs 100';
+
+  //   expect(vendingMachine.deposit(100)).toEqual(displaysAmount);
+  // });
+
+  it('Display how much money was deposited and only accepts bills 10, 20, 50, 100, 500', () => {
+    const vendingMachine = new Machine();
+
+    expect(vendingMachine.deposit(10)).toEqual('You have deposited Rs 10');
+    expect(vendingMachine.deposit(20)).toEqual('You have deposited Rs 30');
+    expect(vendingMachine.deposit(50)).toEqual('You have deposited Rs 80');
+    expect(vendingMachine.deposit(100)).toEqual('You have deposited Rs 180');
+    expect(vendingMachine.deposit(500)).toEqual('You have deposited Rs 680');
+  });
+
+  it('Will not accepts bills other than 10, 20, 50, 100, 500', () => {
+    const vendingMachine = new Machine();
+    const displayMessage = "Invalid bill type";
+    expect(() => vendingMachine.deposit(30)).toThrow(displayMessage);
+  });
+
+  it('This item is unavailable', () => {
+    const vendingMachine = new Machine();
+    const displayMessage = 'The item you selected is unavailable';
+
+    expect(vendingMachine.selectItem("soda")).toBe(displayMessage);
+  });
+
+  it('This deposit is insufficient', () => {
+    const vendingMachine = new Machine();
+    const nuts = {
+      name: 'macadamia nuts',
+      price: 250
+    }
+    const soda = {
+      name: 'soda',
+      price: 200
+    }
+
+    // exercise
+    vendingMachine.stock([nuts, soda]);
+    vendingMachine.deposit(50);
+
+    expect(vendingMachine.selectItem("macadamia nuts")).toBe('Your deposit is insufficient. Please add Rs 200 for this item');
+    expect(vendingMachine.selectItem("soda")).toBe('Your deposit is insufficient. Please add Rs 150 for this item');
+  })
+
+
 })
